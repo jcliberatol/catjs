69d6b031817e2e3b5e1d6dd0bd9a2734f065a5e0
Juan Camilo Liberato <jcliberatol@unal.edu.co>
2015-06-13 20:23:25 -0500

    ready to add database in server when submitting items

--- catjs-meteor/client/analyst.js
+++ catjs-meteor/client/analyst.js
@@ -1,3 +1,5 @@
+GUI$arrays = new Mongo.Collection("gui.arrays");
+
 Template.wd_analyst_navbar.events({
 	"click .pure-menu-item":function(event) {
 		var targetid = event.target.id;
@@ -5,6 +7,16 @@ Template.wd_analyst_navbar.events({
 		Session.set('wd_analyst_viewport_window',targetid);
 		console.log("wd_analyst_viewport_window","enabled",targetid);
 		$(event.target).addClass("pure-menu-selected");
+	}
+});
+
+Template.wd_analyst_items_view.events({
+	"click .pure-item": function(event) {
+		$(".pure-item-selected").removeClass("pure-item-selected");
+		var targetid = '[title='+'"'+event.target.title+'"]'
+		Session.set('wd_analyst_item',event.target.title);
+		$(targetid+".pure-item").addClass("pure-item-selected");
+		return false;
 	}
 });
 
@@ -50,15 +62,198 @@ Template.wd_analyst_items_view.helpers({
 			description : "Un item sencillo",
 			createdBy : "Juanito",
 	},
+
+	selected_item2 : function(){
+		var dct = GUI$arrays._collection.findOne({name : "options"});
+		if (dct){opt = dct.options;}
+		else {opt = [];}
+		var dct = GUI$arrays._collection.findOne({name : "tags"});
+		if (dct){tgs = dct.tags;}
+		else {tgs = [];}
+		return {
+			name: "historia1",
+			text: "Si napoleon bonaparte conquistara america hoy en dia.",
+			question: "¿Como le iria?",
+			options : opt,
+			selected_option : "bien",
+			iframe : "none",
+			tags : tgs,
+			description : "Un item sencillo",
+			createdBy : "Juanito",
+		}
+	},
 });
 
 Template.wd_analyst_item_edit.events({
-
+	"submit .pure-form" : function(event){
+		return false;
+	},
+	"click .button-tag-remove":function(event){
+		rem = event.target.name;
+		var targetid = '[name='+'"'+rem+'"]'
+		$(targetid+'.pure-button-tag').remove();
+		//renegotiate with db
+		var tags = Array();
+		$('.itemtags').each(
+			function(){
+				tags.push(this.value);
+			}
+		);
+		GUI$arrays._collection.update({name : "tags"},{
+				name : "tags",
+				tags : tags,
+			},
+			{
+				upsert : true
+			}
+		);
+		return false
+	},
+	"click .button-option-remove":function(event){
+		rem = event.target.name;
+		var targetid = '[name='+'"'+rem+'"]'
+		$(targetid+'.pure-button-option').remove();
+		//renegotiate with db
+		var options = Array();
+		$('.itemopti').each(
+			function(){
+				options.push(this.value);
+			}
+		);
+		GUI$arrays._collection.update({name : "options"},{
+				name : "options",
+				options : options,
+			},
+			{
+				upsert : true
+			}
+		);
+		return false
+	},
+	"click .option-add-button":function(event){
+		//Current options must go to the DB first
+		var options = Array();
+		$('.itemopti').each(
+			function(){
+				options.push(this.value);
+			}
+		);
+		options.push("");
+		GUI$arrays._collection.update({name : "options"},{
+				name : "options",
+				options : options,
+			},
+			{
+				upsert : true
+			}
+		);
+		return false;
+	},
+	"click .tag-add-button": function(event){
+		//Current options must go to the DB first
+		var tags = Array();
+		$('.itemtags').each(
+			function(){
+				tags.push(this.value);
+			}
+		);
+		tags.push("");
+		GUI$arrays._collection.update({name : "tags"},{
+				name : "tags",
+				tags : tags,
+			},
+			{
+				upsert : true
+			}
+		);
+		return false;
+	},
 	"click .pure-button-option":function(event) {
+		//Save options in database to name reaction
+		var options = Array();
+		$('.itemopti').each(
+			function(){
+				options.push(this.value);
+			}
+		);
+		//
+		GUI$arrays._collection.update({name : "options"},{
+				name : "options",
+				options : options,
+			},
+			{
+				upsert : true
+			}
+		);
 		$(".pure-button-option-selected").removeClass("pure-button-option-selected");
 		var targetid = '[name='+'"'+this+'"]'
 		$(targetid+".pure-button-option").addClass("pure-button-option-selected");
 		return false;
-	}	
+	},
+	"click .pure-button-tag":function(event) {
+		//Save options in database to name reaction
+		var tags = Array();
+		$('.itemtags').each(
+			function(){
+				tags.push(this.value);
+			}
+		);
+		//
+		GUI$arrays._collection.update({name : "tags"},{
+				name : "tags",
+				tags : tags,
+			},
+			{
+				upsert : true
+			}
+		);
+		$(".pure-button-tag-selected").removeClass("pure-button-tag-selected");
+		var targetid = '[name='+'"'+this+'"]'
+		$(targetid+".pure-button-tag").addClass("pure-button-tag-selected");
+		return false;
+	},	
+	"click .item-button-finish":function(event) {
+		//Let's bring everything boys, here's the market list :
+		/*
+		Nombre del item   class = .itemname
+		Descripcion       class = .itemdesc
+		Texto                     .itemtext
+		Pregunta                  .itemques
+		Opciones                  .itemopti
+		Etiquetas                 .itemtags
 
+		Autofills :
+		CreatedBy :
+		CreatedAt :
+		*/
+		itemname = $('.itemname')[0].value;
+		itemdesc = $('.itemdesc')[0].value;
+		itemtext = $('.itemtext')[0].value;
+		itemques = $('.itemques')[0].value;
+		console.log(itemname);
+		console.log(itemdesc);
+		console.log(itemtext);
+		console.log(itemques);
+		var options = Array();
+		$('.itemopti').each(
+			function(){
+				options.push(this.value);
+			}
+		);
+		console.log(options);
+		var tags = Array();
+		$('.itemtags').each(
+			function(){
+				tags.push(this.value);
+			}
+		);
+		console.log(tags);
+		CreatedAt = Date();
+		console.log(CreatedAt);
+
+		//At this point , get ready to push to the moon.
+
+
+
+	}
 });
